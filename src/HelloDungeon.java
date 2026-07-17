903509bd9782ab91c688b40a173f6391624ed38b
Mark Mooibroek <mark@mooibroek.dev>
2026-07-17 17:45:46 +0200

    Added a lamp

--- src/HelloDungeon.java
+++ src/HelloDungeon.java
@@ -3,8 +3,8 @@ public class HelloDungeon {
         System.out.println("=== J U G O ===");
         System.out.println("You stand at the entrance of the dungeon.");
         System.out.println("===============================");
-        System.out.println("===============================");
-        System.out.println("============|-----|============");
+        System.out.println("====**=========================");
+        System.out.println("====**======|-----|============");
         System.out.println("============|     |============");
         System.out.println("============|     |============");
     }
