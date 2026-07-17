1155e9bc1b260829e8e4210e7265d5972ae8cefa
Mark Mooibroek <mark@mooibroek.dev>
2026-07-17 17:28:23 +0200

    First program: dungeon banner

---
+++ src/HelloDungeon.java
@@ -0,0 +1,11 @@
+public class HelloDungeon {
+    public static void main(String[] args) {
+        System.out.println("=== J U G O ===");
+        System.out.println("You stand at the entrance of the dungeon.");
+        System.out.println("===============================");
+        System.out.println("===============================");
+        System.out.println("============|-----|============");
+        System.out.println("============|     |============");
+        System.out.println("============|     |============");
+    }
+}
