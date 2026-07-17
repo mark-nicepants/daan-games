dafa460d037e80383f2e9e9155eb88f174c387a3
Mark Mooibroek <mark@mooibroek.dev>
2026-07-17 18:51:24 +0200

    stage C

--- src/DoorRiddle.java
+++ src/DoorRiddle.java
@@ -1,26 +1,35 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class DoorRiddle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        
-        
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("The door whispers: who approaches?");
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
         System.out.println("Welcome, " + name + ".");
-        
         int secret = 37;
-        System.out.println("I demand a password to enter the dungeon.");
-        int number = scanner.nextInt();
-        if (number > 37) {
-            System.out.println("Too high! The door remains closed.");
-        } else if (number < 37) {
-            System.out.println("Too low! The door remains closed.");
-        } else if (number == 37) {
-            System.out.println("The door creaks open.");
-        } 
+        int number = 0;
+        int attemps = 0;
+        while (number != secret) {
+
+            System.out.println("I demand a password to enter the dungeon.");
+
+            number = scanner.nextInt();
+
+            if (number > 37) {
+                System.out.println("Too high! The door remains closed.");
+                attemps  += 1;
+            } else if (number < 37) {
+                System.out.println("Too low! The door remains closed.");
+                attemps += +1;
+            } else if (number == 37) {
+                System.out.println("The door creaks open.");
+                attemps += +1;
+                System.out.println("you tried " + attemps + " times");
+            }
+        }
+
     }
 }
