df36b23d226dcb5e96d08c78284d53bef78f055b
Mark Mooibroek <mark@mooibroek.dev>
2026-07-17 18:28:01 +0200

    stage B

--- src/DoorRiddle.java
+++ src/DoorRiddle.java
@@ -4,10 +4,23 @@ public class DoorRiddle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
         
+        
+
         System.out.print("The door whispers: who approaches?");
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
         System.out.println("Welcome, " + name + ".");
+        
+        int secret = 37;
+        System.out.println("I demand a password to enter the dungeon.");
+        int number = scanner.nextInt();
+        if (number > 37) {
+            System.out.println("Too high! The door remains closed.");
+        } else if (number < 37) {
+            System.out.println("Too low! The door remains closed.");
+        } else if (number == 37) {
+            System.out.println("The door creaks open.");
+        } 
     }
 }
