45a86a8d69b7e833f3feb87e8b63abd6357a7920
Mark Mooibroek <mark@mooibroek.dev>
2026-07-18 19:38:32 +0200

    0.4 Stage B

--- src/DungeonGame.java
+++ src/DungeonGame.java
@@ -2,7 +2,12 @@ public class DungeonGame {
     public static void main(String[] args) {
         Player hero = new Player("Daan", 20);
         System.out.println(hero.name + " enters the dungeon with " + hero.health + " HP.");
+
+        hero.printStatus();
+        hero.takeDamage(5);
+        hero.printStatus();
     }
+
 }
 
 class Player {
@@ -12,5 +17,15 @@ class Player {
     Player(String startName, int startHealth) {
         name = startName;
         health = startHealth;
+    }
+
+    void printStatus() {
+        System.out.println(name + ": " + health + "HP");
+    }
+
+    void takeDamage(int amount) {
+        health -= amount;
+        System.out.println(name + " took " + amount + " damage. He has " + health + " left remaining.");
+
     }
 }
