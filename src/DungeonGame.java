33f3ad7ae1b3db7cd90b122117209af2267d0469
Mark Mooibroek <mark@mooibroek.dev>
2026-07-18 19:58:58 +0200

    0.4 Stage C

--- src/DungeonGame.java
+++ src/DungeonGame.java
@@ -3,9 +3,14 @@ public class DungeonGame {
         Player hero = new Player("Daan", 20);
         System.out.println(hero.name + " enters the dungeon with " + hero.health + " HP.");
 
+        Monster zombie = new Monster("zombie", 20, 5);
+
         hero.printStatus();
         hero.takeDamage(5);
         hero.printStatus();
+
+        zombie.printStatus();
+    
     }
 
 }
@@ -14,18 +19,39 @@ class Player {
     String name;
     int health;
 
-    Player(String startName, int startHealth) {
-        name = startName;
-        health = startHealth;
+    Player(String name, int health) {
+        this.name = name;
+        this.health = health;
     }
 
     void printStatus() {
-        System.out.println(name + ": " + health + "HP");
+        System.out.println(name + ": " + health + " HP");
     }
 
     void takeDamage(int amount) {
         health -= amount;
         System.out.println(name + " took " + amount + " damage. He has " + health + " left remaining.");
 
+    }
+}
+
+class Monster {
+    String name;
+    int health;
+    int damage;
+
+    public Monster(String name, int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    void printStatus() {
+        System.out.println(name + ": " + health + " HP");
+    }
+
+    void takeDamage(int amount) {
+        health -= amount;
+        System.out.println(name + " took " + amount + " damage. He has " + health + " left remaining.");
     }
 }
